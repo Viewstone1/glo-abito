43c61c16df1d30f043217969343a72accf73eb37
Ivan <86512502+Viewstone1@users.noreply.github.com>
2021-08-17 16:05:50 +0300

    Пытался сделать адаптацию на вторую страницу, не закончил слайдер

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
+
+        // swiper
 var swiper = new Swiper(".mySwiper", {
         spaceBetween: 10,
         slidesPerView: 6,
         freeMode: true,
         watchSlidesVisibility: true,
         watchSlidesProgress: true,
+        breakpoints: {
+          576: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            spaceBetween: 30,
+            slidesPerView: 4,
+            width: auto,
+          },
+          968: {
+            slidesPerView: 6,
+            wi
+          },
+        }
       });
+
       var swiper2 = new Swiper(".mySwiper2", {
         spaceBetween: 10,
         thumbs: {
           swiper: swiper,
         },
       });
+
       const menuBtn = document.querySelector('.menu-button');
       const menu = document.querySelector('.navbar-panel');
       const menuCloseBtn = document.querySelector('.close-menu');
